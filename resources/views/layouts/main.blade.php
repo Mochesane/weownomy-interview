fd0625c195fabc73fdda621508cef165c52506ea
Mochesane Kheekhe <mpalilabmannkheekhe@gmail.com>
2021-10-22 12:58:42 +0200

    first commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Movie Listing</title>
+
+    <link rel="stylesheet" href="/css/main.css">
+    <livewire:styles>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+</head>
+<body class="font-sans bg-gray-900 text-white">
+    <nav class="border-b border-gray-800">
+        <div class="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between px-4 py-6">
+            <ul class="flex flex-col md:flex-row items-center">
+                <li class="md:ml-16 mt-3 md:mt-0">
+                    <a href="{{ route('movies.index') }}" class="hover:text-gray-300">Movies</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    @yield('content')
+    
+    @yield('scripts')
+</body>
+</html>
